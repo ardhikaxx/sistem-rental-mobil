a5578703f0022c94aca85c7699dc754fe6ac241b
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-09 22:00:58 +0700

    fix: penyesuaian user factory dengan kolom pin dan role

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,8 +4,6 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends Factory<User>
@@ -26,10 +24,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'username' => fake()->unique()->userName(),
+            'pin' => bcrypt('1234'),
+            'phone' => fake()->phoneNumber(),
+            'role' => 'employee',
+            'is_active' => true,
         ];
     }
 
