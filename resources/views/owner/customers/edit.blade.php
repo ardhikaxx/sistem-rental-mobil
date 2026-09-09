141395462c0f03057ebc48687ccae246331c3d66
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-09 22:02:24 +0700

    feat: formulir pembaruan data pelanggan

---
+++ resources/views/owner/customers/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+@section('title', 'Edit Pelanggan')
+
+@section('breadcrumb')
+<li class="breadcrumb-item"><a href="{{ route('owner.customers.index') }}">Pelanggan</a></li>
+<li class="breadcrumb-item active">Edit {{ $customer->name }}</li>
+@endsection
+
+@section('content')
+<div class="page-header"><h4>Edit Pelanggan</h4></div>
+<div class="card">
+    <div class="card-body">
+        <form method="POST" action="{{ route('owner.customers.update', $customer) }}">
+            @csrf @method('PUT')
+            <div class="row g-3">
+                <div class="col-md-6"><label class="form-label">Nama Lengkap <span class="text-danger">*</span></label><input type="text" name="name" class="form-control" value="{{ old('name', $customer->name) }}" required></div>
+                <div class="col-md-6"><label class="form-label">Telepon <span class="text-danger">*</span></label><input type="text" name="phone" class="form-control" value="{{ old('phone', $customer->phone) }}" required></div>
+                <div class="col-12"><label class="form-label">Alamat</label><textarea name="address" class="form-control" rows="2">{{ old('address', $customer->address) }}</textarea></div>
+                <div class="col-md-4"><label class="form-label">Jenis Identitas</label><input type="text" name="identity_type" class="form-control" value="{{ old('identity_type', $customer->identity_type) }}"></div>
+                <div class="col-md-4"><label class="form-label">No. Identitas</label><input type="text" name="identity_number" class="form-control" value="{{ old('identity_number', $customer->identity_number) }}"></div>
+                <div class="col-md-4"><label class="form-label">No. SIM</label><input type="text" name="sim_number" class="form-control" value="{{ old('sim_number', $customer->sim_number) }}"></div>
+                <div class="col-md-6"><label class="form-label">Kontak Darurat (Nama)</label><input type="text" name="emergency_contact_name" class="form-control" value="{{ old('emergency_contact_name', $customer->emergency_contact_name) }}"></div>
+                <div class="col-md-6"><label class="form-label">Kontak Darurat (Telepon)</label><input type="text" name="emergency_contact_phone" class="form-control" value="{{ old('emergency_contact_phone', $customer->emergency_contact_phone) }}"></div>
+            </div>
+            <div class="mt-3">
+                <button type="submit" class="btn btn-primary"><i class="fas fa-save me-1"></i>Simpan</button>
+                <a href="{{ route('owner.customers.show', $customer) }}" class="btn btn-secondary">Batal</a>
+            </div>
+        </form>
+    </div>
+</div>
+@endsection
